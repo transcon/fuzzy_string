58f2078fe330c215bcb72e44e259a6b847162a87
Chris Moody <cmoody@transcon.com>
2017-04-06 10:28:29 -0700

    escape special characters

--- lib/fuzzy_string/adjusted_score.rb
+++ lib/fuzzy_string/adjusted_score.rb
@@ -1,8 +1,8 @@
 class FuzzyString::AdjustedScore
   def self.rank(first,second) new(first,second).rank end
   def initialize(first,second)
-    @first  = first
-    @second = second
+    @first  = first.length < second.length ? first  : second
+    @second = first.length < second.length ? second : first
   end
   def rank
     return 0              if (@first == @second)
@@ -12,9 +12,8 @@ class FuzzyString::AdjustedScore
   end
   def adjusted_levenschtein_distance
     special_chars = [")", "(", "]", "[", "}", "{", ".", "?", "+", "*"]
-    pieces = @first.split(/#{@second.chars.to_a.map do |el|
-      special_chars.includes?(el) ? "\\"+el : el
-    end.join('(.*?)')}/i)
+    regex = /#{@second.chars.to_a.map{|el| special_chars.includes?(el) ? "\\"+el : el}.join('(.*?)')}/i
+    pieces = @first.split(regex)
     score  = pieces[0][-1] == ' ' ? -1 : 0
     score += pieces[0..@second.length - 1].uniq == [''] ? -@first.length.to_f / 2 : 0
     score += (pieces[@second.length] || [])[0] == ' ' ? -@first.length.to_f / 2 : 0
